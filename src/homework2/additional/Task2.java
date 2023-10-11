86b00d06ddbd099ba4ecb32e3e8cf5de1eb8af0c
Yauheni Vysotski <yauhenivysotski@gmail.com>
2023-10-11 18:58:56 +0200

    add homework1 and renamed homework2

--- src/homework2/additional/Task2.java
+++ src/homework2/additional/Task2.java
@@ -7,23 +7,16 @@ import java.util.Scanner;
 //Напишите программу, в которую пользователь вводит сумму вклада и количество месяцев. А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
 //Для вычисления суммы с учетом процентов используйте цикл for. Пусть сумма вклада будет представлять тип float.
 public class Task2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter sum: ");
         float sum = scanner.nextFloat();
         System.out.println("Enter months amount: ");
         int month = scanner.nextInt();
-        for (int i = 0; i < month; i++){
+        for (int i = 0; i < month; i++) {
             sum = sum + (sum * 0.07f);
         }
         System.out.println("Your deposit after " + month + " month = " + sum);
-
-
-
-
-
-
-
 
 
     }
