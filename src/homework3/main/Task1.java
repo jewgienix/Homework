4e235ffe4e19bbce3524d42c62b1374d5e29015f
Yauheni Vysotski <yauhenivysotski@gmail.com>
2023-10-18 17:46:43 +0200

    add homework3

--- src/homework3/main/Task1.java
+++ src/homework3/main/Task1.java
@@ -7,23 +7,36 @@ import java.util.Scanner;
 //1. Создайте массив целых чисел. Удалите все вхождения заданного числа из массива.
 //Пусть число задается с консоли (класс Scanner). Если такого числа нет - выведите сообщения об этом.
 //В результате должен быть новый массив без указанного числа.
-public class Task1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your number");
-        int a = scanner.nextInt();
-        int[] array = new int[]{2, 6, 18, 79, 145, 89, 634, 7940, 357};
-        boolean isExist = false;
-        for (int i = 0; i < array.length; i++) {
-            if (a == array[i]) {
-                switch (array[i] = 0){
+public class Task1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number from 10 to 6: ");
+        int[] array = new int[]{10,9,8,7,6};
+        int z = 0;
+        if (input.hasNextInt())
+        {int q = input.nextInt();
+            for (int i = 0; i < array.length; i++)
+            {
+                if (array[i]==q)
+                {
+                    array[i]=0;
                 }
-                isExist = true;
+                else
+                {
+                    z++;
+                }
+            }
+            if (z==5)
+            {
+                System.out.println("This number is not in the array!");
             }
         }
-        if(!isExist){
-            System.out.println("enter correct number");
+        else
+        {
+            System.out.println("Error, enter the correct number: ");
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println("Result: " + Arrays.toString(array));
     }
 }
