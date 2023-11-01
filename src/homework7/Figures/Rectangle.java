c520a7ec5cbe3e0030486aa87798d8db5a38c09f
Yauheni Vysotski <yauhenivysotski@gmail.com>
2023-11-01 10:41:31 +0100

    add homework7

---
+++ src/homework7/Figures/Rectangle.java
@@ -0,0 +1,20 @@
+package homework7.Figures;
+
+public class Rectangle extends Figure {
+    private double width, height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + height) * 2;
+    }
+}
